bb11fc672e6fdd0523546c98ca50b520e0e68829
Jan Strauß <jan@over9000.eu>
2015-03-14 19:43:58 +0100

    cleanup

--- src/main/java/eu/over9000/skadi/util/NotificationUtil.java
+++ src/main/java/eu/over9000/skadi/util/NotificationUtil.java
@@ -3,7 +3,6 @@ package eu.over9000.skadi.util;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
@@ -15,38 +14,33 @@ import eu.over9000.skadi.model.StateContainer;
 import eu.over9000.skadi.service.ImageRetrievalService;
 
 public class NotificationUtil {
-	
+
 	private static final Stage dummyStage = NotificationUtil.buildDummyStage();
-	
+
 	public static void showOnlineNotification(final Channel channel) {
-		
+
 		if (StateContainer.getInstance().isDisplayNotifications()) {
 			final ImageRetrievalService service = new ImageRetrievalService(channel.getLogoURL(), 72, 72);
 			service.setOnSucceeded(event -> {
 				final ImageView img = (ImageView) event.getSource().getValue();
-				
+
 				Notifications.create().text(channel.getName() + " went live").title("Skadi")
-				        .hideAfter(Duration.seconds(5)).graphic(img).show();
+				.hideAfter(Duration.seconds(5)).graphic(img).show();
 			});
 			service.start();
 		}
 	}
-	
+
 	public static void onShutdown() {
 		NotificationUtil.dummyStage.hide();
 	}
-
+	
 	private static Stage buildDummyStage() {
 		final Stage dummy = new Stage(StageStyle.UTILITY);
-		final Pane root = new Pane();
-		root.setStyle("-fx-background-color: TRANSPARENT");
-		final Scene scene = new Scene(root, 1, 1);
-		scene.setFill(Color.TRANSPARENT);
-		dummy.setScene(scene);
-		dummy.setMaxHeight(0);
-		dummy.setMaxWidth(0);
+		dummy.setScene(new Scene(new Pane(), 1, 1));
+		dummy.setMaxHeight(1);
+		dummy.setMaxWidth(1);
 		dummy.setX(Double.MAX_VALUE - 1);
-		dummy.toBack();
 		dummy.show();
 		return dummy;
 	}
