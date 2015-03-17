03331163c699f74a46c817edbed481f2d1f09abe
Jan Strauß <jan@over9000.eu>
2015-03-17 07:58:47 +0100

    added drag and drop support, you can now drag twitch urls/usernames into skadi and they will be added

--- src/main/java/eu/over9000/skadi/util/StringUtil.java
+++ src/main/java/eu/over9000/skadi/util/StringUtil.java
@@ -1,8 +1,13 @@
 package eu.over9000.skadi.util;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtil {
+
+	final static Pattern urlPattern = Pattern.compile("http(s)*://(www\\.)*twitch\\.tv/(\\w+{2,25})(/.*)?");
+
 	public static boolean containsIgnoreCase(final List<String> list, final String lookup) {
 		for (final String current : list) {
 			if (current.equalsIgnoreCase(lookup)) {
@@ -10,5 +15,15 @@ public class StringUtil {
 			}
 		}
 		return false;
+	}
+	
+	public static String extractUsernameFromURL(final String url) {
+		final Matcher urlMatcher = StringUtil.urlPattern.matcher(url);
+
+		if (urlMatcher.matches()) {
+			return urlMatcher.group(3);
+		}
+		
+		return null;
 	}
 }
