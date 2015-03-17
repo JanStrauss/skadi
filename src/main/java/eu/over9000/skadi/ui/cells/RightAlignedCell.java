4a23acd59fce4a2c48d541d3150cc361bf68a1e5
Jan Strauß <jan@over9000.eu>
2015-03-17 09:43:28 +0100

    add padding to right aligned cells

--- src/main/java/eu/over9000/skadi/ui/cells/RightAlignedCell.java
+++ src/main/java/eu/over9000/skadi/ui/cells/RightAlignedCell.java
@@ -1,5 +1,6 @@
 package eu.over9000.skadi.ui.cells;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.TableCell;
 import eu.over9000.skadi.model.Channel;
@@ -7,18 +8,19 @@ import eu.over9000.skadi.model.Channel;
 public class RightAlignedCell<T> extends TableCell<Channel, T> {
 	public RightAlignedCell() {
 		this.setAlignment(Pos.CENTER_RIGHT);
+		this.setPadding(new Insets(0, 5, 0, 0));
 	}
-
+	
 	@Override
 	protected void updateItem(final T item, final boolean empty) {
 		super.updateItem(item, empty);
-		
-		if (empty || item == null) {
+
+		if (empty || (item == null)) {
 			this.setText(null);
 			this.setGraphic(null);
 		} else {
 			this.setText(item.toString());
 		}
-		
+
 	}
 }
--- src/main/java/eu/over9000/skadi/ui/cells/UptimeCell.java
+++ src/main/java/eu/over9000/skadi/ui/cells/UptimeCell.java
@@ -1,26 +1,28 @@
 package eu.over9000.skadi.ui.cells;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.TableCell;
 import eu.over9000.skadi.model.Channel;
 import eu.over9000.skadi.util.TimeUtil;
 
 public class UptimeCell extends TableCell<Channel, Long> {
-
+	
 	public UptimeCell() {
 		this.setAlignment(Pos.CENTER_RIGHT);
+		this.setPadding(new Insets(0, 5, 0, 0));
 	}
-
+	
 	@Override
 	protected void updateItem(final Long item, final boolean empty) {
 		super.updateItem(item, empty);
-		
-		if (empty || item == null) {
+
+		if (empty || (item == null)) {
 			this.setText(null);
 			this.setGraphic(null);
 		} else {
 			this.setText(TimeUtil.getDurationBreakdown(item, false));
 		}
-		
+
 	}
 }
